31f265318ca0134d4874fc927599dbba44d0f413
Robert Bowers <32080792+phdoc0@users.noreply.github.com>
2021-02-21 17:52:51 -0500

    Question 12 posted late

---
+++ week3Question12/src/week3Question12/week3Question12.java
@@ -0,0 +1,36 @@
+package week3Question12;
+
+import testStuff.retun;
+
+public class week3Question12 {
+
+	public static void main(String[] args) {
+
+		//willBuyMilk is true if moneyInPocket is > 10.50
+		//if(moneyInPocket > 10.50 && isHotOutside);
+		
+		boolean isHotOutside = true;
+		double moneyInPocket = 20.00;
+		
+		boolean willBuyMilk = (moneyInPocket > 10.50 && isHotOutside);
+		
+		System.out.println(willBuyMilk);
+		
+	}
+		
+		public static double willBuyMilk(boolean isHotOutside, double moneyInPocket) {
+			if (moneyInPocket > 10.50 && isHotOutside);
+			
+		}
+			return willBuyMilk;
+			
+			
+			
+			//boolean = willBuyMilk; 
+			//retun willBuyMilk; 
+
+		
+		
+	
+
+}
